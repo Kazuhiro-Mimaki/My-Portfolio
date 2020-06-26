a6054cf15b0df600b20d76be28765d9f7d4dce85
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2020-06-27 08:42:07 +0900

    implement header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div> <!-- <div id="app"> -->
+    <Header></Header>
+    <!-- <div id="nav"> -->
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    <!-- </div> -->
+    <!-- <router-view/> -->
   </div>
 </template>
+
+<script>
+import 'normalize.css'
+import Header from './components/Header'
+
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
 
 <style>
 #app {
