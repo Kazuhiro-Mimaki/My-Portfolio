dc7cf2af36eadd2d287e84c9446f0521a0d8281e
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2020-12-16 23:14:15 +0900

    fix work

---
+++ src/components/atoms/Card/Component.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: ['src', 'href', 'title', 'detail', 'languages'],
+});
+</script>
+
+<template>
+  <a :class="$style.component" :href="href" target="_blank">
+    <img :src="src" :alt="title" />
+    <div :class="$style.content">
+      <h3>{{ title }}</h3>
+      <p :class="$style.detail">
+        {{ detail }}
+      </p>
+      <p>{{ languages }}</p>
+    </div>
+  </a>
+</template>
+
+<style lang="scss" module>
+.component {
+  display: block;
+  text-decoration: none;
+
+  img {
+    width: 100%;
+    border-bottom: solid 1px #ccc;
+  }
+
+  .content {
+    padding: 0 20px 20px;
+
+    .detail {
+      text-align: left;
+    }
+
+    p {
+      color: rgb(50, 50, 50);
+    }
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .component {
+    .content {
+      p {
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
--- src/components/molecules/Works/Component.vue
+++ src/components/molecules/Works/Component.vue
@@ -1,112 +1,106 @@
-<script>
-import pageTitle from '@/components/atoms/SectionTitle/Component';
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
 
-export default {
+export default defineComponent({
   components: {
-    pageTitle,
+    Section: require('@/components/atoms/Section/Component.vue').default,
+    SectionTitle: require('@/components/atoms/SectionTitle/Component.vue').default,
+    Card: require('@/components/atoms/Card/Component.vue').default,
   },
-};
+  setup: () => {
+    const works = [
+      {
+        src: require('./images/geekhub.png'),
+        href: 'https://geekhubhack.herokuapp.com/',
+        title: 'GEEK HUB',
+        detail:
+          'GEEK HUBは開発物を気軽に投稿できるサービスです。WebAPIの勉強のためにGitHubAPIを使いたくて, このアプリを作成しました。まだ不十分な部分はこれから実装していきます。',
+        languages: 'Ruby on Rails, Bootstrap, GitHub API, Heroku',
+      },
+      {
+        src: require('./images/portfolio.png'),
+        href: 'https://mk-portfolio-site.netlify.app/',
+        title: 'Portfolio',
+        detail:
+          'このポートフォリオサイトです。色・レイアウト共にシンプルさを意識し, レスポンシブ対応まで頑張りました。簡易ですが, JavaScript, Vue.jsのいい勉強になりました。',
+        languages: 'JavaScript, TypeScript, Vue.js, Netlify',
+      },
+      {
+        src: require('./images/blog.png'),
+        href: 'https://b1essk.com/',
+        title: 'Blog',
+        detail:
+          '技術ブログです。Hugoの"Zzo"というテーマを使っています。tcardgenでOGPを自動生成できるようにし, ビルド&デプロイはGitHub Actionsで自動化しました。',
+        languages: 'Hugo(Zzo), firebase(hosting)',
+      },
+      {
+        src: require('./images/buzzrita.png'),
+        href: 'https://twitter.com/buzzrita/',
+        title: 'Buzzrita',
+        detail:
+          '"エンジニア"や"プログラミング"といったキーワードに紐づくLGTM数の多い記事を毎日2回呟いてくれるTwitterボットです。データはスクレイピングで取得し, Herokuのtime schedulerを利用してプログラムを定期実行しています。',
+        languages: 'Python, Twitter API, heroku',
+      },
+    ];
+
+    return { works };
+  },
+});
 </script>
 
 <template>
-  <div id="works">
-    <pageTitle title="Works"></pageTitle>
-    <ul>
-      <li>
-        <a href="https://geekhubhack.herokuapp.com/">
-          <img src="@/assets/geekhub.png" />
-          <div class="content">
-            <h3>GEEK HUB</h3>
-            <p class="detail">
-              GEEK HUBは開発物を気軽に投稿できるサービスです。WebAPIの勉強のためにGitHub
-              APIを使いたくて、このアプリを作成しました。まだ不十分な部分はこれから実装していきます。
-            </p>
-            <p>Ruby on Rails, Bootstrap, GitHub API, Heroku</p>
-          </div>
-        </a>
-      </li>
-      <li>
-        <a href="https://mk-portfolio-site.netlify.app/">
-          <img src="@/assets/portfolio.png" />
-          <div class="content">
-            <h3>Portfolio</h3>
-            <p class="detail">
-              このポートフォリオサイトです。色・レイアウト共にシンプルさを意識し、レスポンシブ対応まで頑張りました。簡易ですが、JavaScript、Vue.jsのいい勉強になりました。
-            </p>
-            <p>JavaScript, Vue.js, Netlify</p>
-          </div>
-        </a>
-      </li>
-    </ul>
-  </div>
+  <Section :class="$style.component" id="works">
+    <SectionTitle title="Works" />
+    <div :class="$style.cards">
+      <Card
+        :class="$style.card"
+        v-for="(work, i) in works"
+        :key="i"
+        :src="work.src"
+        :href="work.href"
+        :title="work.title"
+        :detail="work.detail"
+        :languages="work.languages"
+      />
+    </div>
+  </Section>
 </template>
 
-<style scoped>
-#works {
-  padding-top: 100px; /* 打ち消し用のパディング */
-  margin-top: -50px; /* 固定ナビの高さ分のネガティブマージン */
-  margin-bottom: 200px;
-}
+<style lang="scss" module>
+.component {
+  .cards {
+    display: flex;
+    justify-content: space-evenly;
+    width: 60%;
+    padding-left: 0;
+    margin: 0 auto;
 
-ul {
-  display: flex;
-  justify-content: space-evenly;
-  width: 60%;
-  padding-left: 0;
-  margin: 0 auto;
-}
+    .card {
+      margin: 0 20px;
+      text-align: center;
+      list-style: none;
+      border-radius: 5px;
+      box-shadow: 0 2px 5px #ccc;
+      transition: all 250ms ease-out;
+      transform-style: flat;
 
-li {
-  margin: 0 20px;
-  text-align: center;
-  list-style: none;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px #ccc;
-  transition: all 250ms ease-out;
-  transform-style: flat;
-}
-
-a {
-  text-decoration: none;
-}
-
-p {
-  color: rgb(50, 50, 50);
-}
-
-img {
-  width: 100%;
-  border-bottom: solid 1px #ccc;
-}
-
-.content {
-  padding: 0 20px 20px;
-}
-
-.detail {
-  text-align: left;
-}
-
-li:hover {
-  box-shadow: 0 10px 15px rgba(31, 31, 31, 0.5);
+      &:hover {
+        box-shadow: 0 10px 15px rgba(31, 31, 31, 0.5);
+      }
+    }
+  }
 }
 
 @media screen and (max-width: 480px) {
-  #works {
-    margin-bottom: 100px;
-  }
+  .component {
+    .cards {
+      display: initial;
+      width: 60%;
 
-  ul {
-    display: initial;
-    width: 60%;
-  }
-
-  li {
-    margin-bottom: 50px;
-  }
-
-  p {
-    font-size: 15px;
+      .card {
+        margin-bottom: 50px;
+      }
+    }
   }
 }
 </style>
